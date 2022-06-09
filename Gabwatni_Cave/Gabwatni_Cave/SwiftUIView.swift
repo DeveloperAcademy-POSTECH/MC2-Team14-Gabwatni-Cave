3b5789a5faf678247f0d78de56822e4a9e6c5137
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2022-06-09 17:41:36 +0900

    test

---
+++ Gabwatni_Cave/Gabwatni_Cave/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  Gabwatni_Cave
+//
+//  Created by 이창형 on 2022/06/09.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("dddddddd")
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
