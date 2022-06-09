5b12e9264e56e7ef803b1010274893ad054fb15b
DK <dk143496@gmail.com>
2022-06-09 19:46:54 +0900

    add StartZone

---
+++ Gabwatni_Cave/Gabwatni_Cave/StartZone/AncestorView.swift
@@ -0,0 +1,20 @@
+//
+//  AncestorView.swift
+//  Gabwatni_Cave
+//
+//  Created by DongKyu Kim on 2022/06/09.
+//
+
+import SwiftUI
+
+struct AncestorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AncestorView_Previews: PreviewProvider {
+    static var previews: some View {
+        AncestorView()
+    }
+}
