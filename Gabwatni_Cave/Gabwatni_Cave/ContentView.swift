70bc9e35edf0a5511460511673b68020e05a0f24
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2022-06-10 16:27:24 +0900

    fix

--- Gabwatni_Cave/Gabwatni_Cave/ContentView.swift
+++ Gabwatni_Cave/Gabwatni_Cave/ContentView.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-//        Text("Hello test")
-//            .padding()
-        DeepDarkView()
+        Text("Hello test")
+            .padding()
+        
    
     }
 }
