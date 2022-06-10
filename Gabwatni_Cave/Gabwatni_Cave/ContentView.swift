1e97b478e0e028912fbb3622e23b99d523de4f9c
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2022-06-10 14:01:25 +0900

    go

--- Gabwatni_Cave/Gabwatni_Cave/ContentView.swift
+++ Gabwatni_Cave/Gabwatni_Cave/ContentView.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-//        Text("Hello test")
-//            .padding()
-        worldMapView()
+        Text("Hello test")
+            .padding()
+   
     }
 }
 
