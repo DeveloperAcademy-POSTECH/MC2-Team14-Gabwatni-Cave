774ffe99020790689e7dd1e423b84140f3c1a2cb
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2022-06-10 13:59:47 +0900

    미니맵, 가방, 지도뷰

--- Gabwatni_Cave/Gabwatni_Cave/DarkZone/DeepDark/DeepDarkView.swift
+++ Gabwatni_Cave/Gabwatni_Cave/DarkZone/DeepDark/DeepDarkView.swift
@@ -8,8 +8,76 @@
 import SwiftUI
 
 struct DeepDarkView: View {
+    @State var showSheet: Bool = false
+    @State var showSheet2: Bool = false
+    @State var transitionView: Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+        ZStack{
+            Rectangle()
+                .edgesIgnoringSafeArea(.all)
+                .transition(AnyTransition.opacity.animation(.easeInOut(duration: 2)))
+                .overlay(
+                    Image("deepDark")
+                        .resizable()
+                        .scaledToFill()
+                        .edgesIgnoringSafeArea(.all)
+                    
+                )
+        
+                
+                    Button{
+                        withAnimation{
+                            transitionView.toggle()}
+                    }label: {
+                        Text("가방")
+                            .foregroundColor(.white)
+                            
+                    }
+                    .padding()
+                    .frame(maxWidth:.infinity, maxHeight: .infinity, alignment:.topTrailing)
+                    
+                
+              
+                //ZStack{
+                if transitionView {
+                    ZStack{
+                        Rectangle()
+                            .ignoresSafeArea()
+                            .frame(width: geometry.size.width/1.3, height: geometry.size.height/1.2, alignment: .center)
+                            .position(x:geometry.size.width/2 , y: geometry.size.height/2)
+                        HStack{
+                        Image("worldmap")
+                            .resizable()
+                            .frame(width: geometry.size.width/3, height: geometry.size.height/6).onTapGesture {
+                                showSheet2.toggle()
+                            }
+                            .fullScreenCover(isPresented: $showSheet2, content:{ worldMapView()})
+
+                            
+                            
+                            
+                            Image("minimap")
+                                .resizable()
+                                .frame(width: geometry.size.width/3, height: geometry.size.height/6)
+                                .onTapGesture {
+                                    showSheet.toggle()
+                                }
+                                .fullScreenCover(isPresented: $showSheet, content:{ MiniMapView()})
+                        }
+                        
+                        
+                            
+                        
+               
+                    }
+                }
+            
+            
+            
+        }
+        
+        }
     }
 }
 
