736cc45340fb950fde485020fb33e1b51e916d0d
DK <dk143496@gmail.com>
2022-06-13 17:00:18 +0900

    Cali commit

--- Gabwatni_Cave/Gabwatni_Cave/StartZone/AncestorView.swift
+++ Gabwatni_Cave/Gabwatni_Cave/StartZone/AncestorView.swift
@@ -7,24 +7,44 @@
 
 import SwiftUI
 import Shimmer
+import AVFoundation
 
 struct AncestorView: View {
     @State private var imageScrollLocation: CGPoint = CGPoint(x: 500, y: 400)
     @State private var lightLocation: CGPoint = CGPoint(x: 400, y: 400)
-    @State private var ancestorLocation: CGPoint = CGPoint(x: 650, y: 550)
+    @State private var ancestorLocation: CGPoint = CGPoint(x: 650, y: 500)
+    
     @State private var textboxState: Bool = false
     @State private var ancestorState: Bool = true
-    @State var flow = 0
-    @State var count = 0
+    @State private var cardViewState: Bool = false
+    
+    @State private var flow = 0
+    @State private var count = 0
+    
+    @State private var str = ""
+    @State private var str2 = "조상님의 지도 발견!"
     
     @State private var presentView: Bool = false
     @State private var showingImage: String =  ""
     
     var body: some View {
+        ZStack {
         if flow == 0 {
             StartZoneStartView
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                        withAnimation(.easeInOut(duration: 2)) {
+                            flow = 1
+                        }
+                             }
+                }
         } else if flow == 1 {
             AncestorPageView
+        }
+        
+        if cardViewState {
+            CardView(imageName: "dongdal", cardState: $cardViewState)
+            }
         }
     }
     
@@ -45,7 +65,7 @@ struct AncestorView: View {
                 }
             } label: {
                 Text("조상님의 안식처")
-                    .font(.system(size: 40))
+                    .font(.custom("Sam3KRFont", size: 40))
                     .foregroundColor(.white)
             }
         }
@@ -55,7 +75,7 @@ struct AncestorView: View {
     private var AncestorPageView: some View {
         ZStack {
             Rectangle()
-                .transition(AnyTransition.opacity.animation(.easeIn(duration: 3)))
+                .transition(AnyTransition.opacity.animation(.easeIn(duration: 2)))
                 .edgesIgnoringSafeArea(.all)
                 .overlay(
                     ZStack {
@@ -65,7 +85,7 @@ struct AncestorView: View {
                             .frame(height: 1300)
                             .position(imageScrollLocation)
                             .onAppear{
-                                withAnimation(.easeIn(duration: 3).delay(1)) {
+                                withAnimation(.easeIn(duration: 3)) {
                                     imageScrollLocation.x = 150
                                 }
                                 
@@ -76,42 +96,32 @@ struct AncestorView: View {
                             .frame(width: 30)
                             .position(lightLocation)
                             .onAppear{
-                                withAnimation(.easeIn(duration: 3).delay(1)) {
+                                withAnimation(.easeIn(duration: 3)) {
                                     lightLocation.x = 50
                                 }
                             }
                         
                         AncestorShimmeringView
-                            .frame(width: 100)
-                            .position(ancestorLocation)
-                            .onAppear{
-                                withAnimation(.easeIn(duration: 3).delay(1)) {
-                                    ancestorLocation.x = 300
-                                }
-                                withAnimation(.easeIn(duration: 1).delay(5)) {
-                                    textboxState.toggle()
-                                }
-                            }
                         
                         if textboxState == true {
-                            ZStack {
-                            Rectangle()
-                                .frame(width: 200, height: 100, alignment: .center)
-                            Text("헉, 이건 혹시 조상님의 흔적?")
-                                    .foregroundColor(.white)
-                            }
+                            textBox(name: "최병호", text: str)
+                                .onAppear {
+                                    ges()
+                                }
                         }
                     }
                 )
-
-            HStack{
-                Button{
+                .onDisappear {
                     lightLocation.x = 400
                     imageScrollLocation.x = 500
                     ancestorLocation.x = 650
-                    textboxState.toggle()
-                    //imageScrollLocation = CGPoint(x: 500, y: 400)
-                    flow = 0
+                }
+
+            HStack{
+                Button{
+                    withAnimation(.easeIn) {
+                    cardViewState.toggle()
+                    }
                 }label: {
                     Image(systemName: "arrowtriangle.right.fill")
                         .resizable()
@@ -126,16 +136,44 @@ struct AncestorView: View {
     
     private var AncestorShimmeringView: some View {
         Button {
-            ancestorState = false
+            withAnimation(.easeInOut) {
+                textboxState = true
+                ancestorState = false
+            }
             // count 증가
         } label: {
-            Image("choi byung-ho")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .shimmering(active: ancestorState, duration: 2.0)
+            if ancestorState {
+                Image("skeletonMapO")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    //.shimmering(active: ancestorState, duration: 2.0)
+            } else {
+                Image("skeletonMapX")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+            }
         }
-        .opacity(ancestorState ? 1 : 0.6)
-        
+        .frame(width: 200, height: 200)
+        .position(ancestorLocation)
+        .onAppear{
+            withAnimation(.easeIn(duration: 3)) {
+                ancestorLocation.x = 300
+            }
+        }
+    }
+    
+    func ges(){
+        str = ""
+        let length = str2.count
+        var index = 0
+        Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true) { (timer) in
+            AudioServicesPlaySystemSound(1306)
+            str += String(str2[str2.index(str2.startIndex, offsetBy: index)])
+            index+=1
+            if index == length{
+                timer.invalidate()
+            }
+        }
     }
 }
 
