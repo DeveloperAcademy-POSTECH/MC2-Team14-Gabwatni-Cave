ed8e54be0b25dc74aa5b9106427a7a016a628ede
DK <dk143496@gmail.com>
2022-06-17 15:53:12 +0900

    modify cardView

--- Gabwatni_Cave/Gabwatni_Cave/Card/CardView.swift
+++ Gabwatni_Cave/Gabwatni_Cave/Card/CardView.swift
@@ -2,7 +2,7 @@
 //  CardView.swift
 //  Gabwatni_Cave
 //
-//  Created by DongKyu Kim on 2022/06/09.
+//  Created by DongKyu Kim on 2022/06/16.
 //
 
 import SwiftUI
@@ -14,8 +14,11 @@ struct CardView: View {
     @State var backDegree = 90.0
     @State var frontDegree = 0.0
     @Binding var cardState: Bool
+    @State private var cardText = ""
     let durationAndDelay : CGFloat = 0.3
     var alpha: Double = 1.0
+    
+    @State private var blinkToggle = false
     
     var body: some View {
         if cardState {
@@ -29,6 +32,35 @@ struct CardView: View {
                         cardState = false
                     }
                 
+                
+                if vm.imageName == "stalactite" {
+                Text("카드를 터치해 뒤집어 보세요!")
+                    .font(.custom("Sam3KRFont", size: 20))
+                    .frame(width: 300, height: 50, alignment: .center)
+                    .foregroundColor(.white)
+                    .position(x: UIScreen.main.bounds.width/2
+                              , y: UIScreen.main.bounds.height/15 * 0.8)
+                    .opacity(blinkToggle ? 1 : 0)
+                    .onAppear(){
+                        withAnimation(.spring().repeatForever()) {
+                            blinkToggle = true
+                        }
+                    }
+                } else if vm.imageName == "salamander" || vm.imageName == "bat" {
+                    Text("카드를 터치하면 실제사진이 나와요!")
+                        .font(.custom("Sam3KRFont", size: 19))
+                        .frame(width: 340, height: 50, alignment: .center)
+                        .foregroundColor(.white)
+                        .position(x: UIScreen.main.bounds.width/2
+                                  , y: UIScreen.main.bounds.height/15 * 0.8)
+                        .opacity(blinkToggle ? 1 : 0)
+                        .onAppear(){
+                            withAnimation(.spring().repeatForever()) {
+                                blinkToggle = true
+                            }
+                        }
+                    }
+                
                 ZStack {
                     CardFront
                     CardBack
@@ -37,33 +69,46 @@ struct CardView: View {
                     flipCard ()
                 }
                 
+//                .position(x: UIScreen.main.bounds.width/2
+//                          , y: UIScreen.main.bounds.height/10 * 3.2)
                 .position(x: UIScreen.main.bounds.width/2
-                          , y: UIScreen.main.bounds.height/10 * 4)
+                                          , y: UIScreen.main.bounds.height/10 * 3.5)
                 
                 
                 // 카드 외부 텍스트 박스
-                textBox(name: "", text: vm.inputString)
+                //textBox(name: "", text: cardText)
+                Text(cardText)
+                    .font(.custom("Sam3KRFont", size: 18))
+                    .lineSpacing(2)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.leading)
+                    .frame(width: 250, height: 200, alignment: .leading)
+                    .overlay(
+                            Image("textbox")
+                                .resizable()
+                                .frame(width: 310, height: 150, alignment: .leading)
+                        )
+                    .position(x: UIScreen.main.bounds.width/2
+                              , y: UIScreen.main.bounds.height/9 * 6.7)
                     .onAppear {
                         vm.loadCardData(imageName: imageName)
-                        vm.talkOnTextBox(stringArray: vm.imageText, inputIndex: vm.stringArrayIndex)
+                        cardText = arrayToString(inputArray: vm.imageText)
                     }
                 
                 
-                if vm.textEnd {
-                    Button{
-                        vm.talkOnTextBox(stringArray: vm.imageText, inputIndex: vm.stringArrayIndex)
-                    } label:{
-                        Text("다음")
-                            .font(.custom("Sam3KRFont", size: 20))
-                            .foregroundColor(.white)
-                            .frame(width: UIScreen.main.bounds.size.width+300, height: UIScreen.main.bounds.size.height+600, alignment: .center)
-                    }
+//                if vm.textEnd {
+//                    Button{
+//                        vm.talkOnTextBox(stringArray: vm.imageText, inputIndex: vm.stringArrayIndex)
+//                    } label:{
+//                        Text("다음")
+//                            .font(.custom("Sam3KRFont", size: 20))
+//                            .foregroundColor(.white)
+//                            .frame(width: UIScreen.main.bounds.size.width+300, height: UIScreen.main.bounds.size.height+600, alignment: .center)
+//                    }
 //                    .position(x: UIScreen.main.bounds.width/16 * 13
 //                              , y: UIScreen.main.bounds.height/15 * 13)
-                    .position(x: UIScreen.main.bounds.width/16 * 13
-                              , y: UIScreen.main.bounds.height/15 * 13)
-                    
-                }
+//
+//                }
                 
             }
             .offset(y: 40)
@@ -181,5 +226,16 @@ struct CardView: View {
                 frontDegree = 0
             }
         }
+    }
+    
+    func arrayToString(inputArray: [String]) -> String {
+        var outputString : String = ""
+        
+        for text in inputArray {
+            outputString += text
+            outputString += ""
+        }
+        
+        return outputString
     }
 }
